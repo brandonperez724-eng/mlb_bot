ee3213dae792ca984fb868a629aae62c18e8aab9
brandonperez724-eng <brandonperez724@gmail.com>
2026-04-02 23:30:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import os
 
 app = Flask(__name__)
 
@@ -9,15 +8,8 @@ def run():
 
     if mode == "picks":
         print("running picks")
-        # call picks function
 
     elif mode == "grading":
         print("running grading")
-        # call grading function
 
     return "OK"
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
