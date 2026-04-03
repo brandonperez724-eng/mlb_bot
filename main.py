4d8321a683cf5b720bc022e781e48df725c8510d
brandonperez724-eng <brandonperez724@gmail.com>
2026-04-03 19:16:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+from mlb_bot_NEW_v2 import run_bot
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    mode = request.args.get("mode", "picks")
+
+    run_bot(mode)
+
     return "OK"
